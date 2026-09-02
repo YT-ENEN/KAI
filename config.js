8e02e373ef67b760a440bb5b87dcea26a2d25063
czlforceofficial1116 <czlforceofficial1116@gmail.com>
2026-09-02 21:34:53 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.STUDIO_CONFIG = {
-  API_URL: "PASTE_YOUR_APPS_SCRIPT_WEB_APP_URL_HERE",
+  API_URL: "https://script.google.com/macros/s/AKfycbyagTSV-0dgyh4mH4utYaWvh-1HrW8YSE4rTBuS2xSoQ4JjUnkcUCZWsgikwUpMqAwl_g/exec",
   APP_NAME: "REINOU STUDIO",
   VERSION: "1.0.0"
 };
